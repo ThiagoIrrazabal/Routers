e6cb7085f3cee2b0efbf394472f71074c1bd3e8b
ThiagoIrrazabal <4920726+ThiagoIrrazabal@users.noreply.github.com>
2025-04-29 16:17:45 -0300

    Ajustes

--- src/Bool.Router.pas
+++ src/Bool.Router.pas
@@ -9,8 +9,6 @@ uses
   Classe.SmartPointer;
 
 type
-  TBoolMethod = TProc<TArray<TValue>>;
-
   IBoolRouter = Interface(IInterface)
     ['{4B319973-EEE9-4B61-A13B-5B16D20A4F4B}']
     procedure Execute(const ACondition: Boolean; const Args: TArray<TValue>);
@@ -19,10 +17,10 @@ type
   TBoolRouter = class(TInterfacedObject, IBoolRouter)
   strict private
   var
-    FListMethods: ISmartPointer<TDictionary<Boolean, TBoolMethod>>;
+    FListMethods: ISmartPointer<TDictionary<Boolean, TProc<TArray<TValue>>>>;
   public
-    constructor Create(const ATrue, AFalse: TBoolMethod);
-    class function New(const ATrue, AFalse: TBoolMethod): IBoolRouter;
+    constructor Create(const ATrue, AFalse: TProc<TArray<TValue>>);
+    class function New(const ATrue, AFalse: TProc<TArray<TValue>>): IBoolRouter;
     procedure Execute(const ACondition: Boolean; const Args: TArray<TValue>);
   End;
 
@@ -30,23 +28,23 @@ implementation
 
 { TBoolRouter }
 
-constructor TBoolRouter.Create(const ATrue, AFalse: TBoolMethod);
+constructor TBoolRouter.Create(const ATrue, AFalse: TProc<TArray<TValue>>);
 begin
-  FListMethods := TSmartPointer<TDictionary<Boolean, TBoolMethod>>.Create(
-    TDictionary<Boolean, TBoolMethod>.Create);
+  FListMethods := TSmartPointer<TDictionary<Boolean, TProc<TArray<TValue>>>>.Create(
+    TDictionary<Boolean, TProc<TArray<TValue>>>.Create);
   FListMethods.Add(True, ATrue);
   FListMethods.Add(False, AFalse);
 end;
 
 procedure TBoolRouter.Execute(const ACondition: Boolean; const Args: TArray<TValue>);
 var
-  lMethod: TBoolMethod;
+  lMethod: TProc<TArray<TValue>>;
 begin
   FListMethods.TryGetValue(ACondition, lMethod);
   lMethod(Args);
 end;
 
-class function TBoolRouter.New(const ATrue, AFalse: TBoolMethod): IBoolRouter;
+class function TBoolRouter.New(const ATrue, AFalse: TProc<TArray<TValue>>): IBoolRouter;
 begin
   Result := Self.Create(ATrue, AFalse);
 end;
