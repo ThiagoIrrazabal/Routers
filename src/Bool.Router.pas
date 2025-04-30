224293e28aabe83bcd0888a5d4edd297e27be20b
ThiagoIrrazabal <4920726+ThiagoIrrazabal@users.noreply.github.com>
2025-04-30 14:20:40 -0300

    New overload

--- src/Bool.Router.pas
+++ src/Bool.Router.pas
@@ -19,8 +19,12 @@ type
   var
     FListMethods: ISmartPointer<TDictionary<Boolean, TProc<TArray<TValue>>>>;
   public
-    constructor Create(const ATrue, AFalse: TProc<TArray<TValue>>);
-    class function New(const ATrue, AFalse: TProc<TArray<TValue>>): IBoolRouter;
+    constructor Create(const ATrue, AFalse: TProc<TArray<TValue>>); overload;
+    constructor Create(const AProc: TProc<TArray<TValue>>;
+      const ACondition: Boolean); overload;
+    class function New(const ATrue, AFalse: TProc<TArray<TValue>>): IBoolRouter; overload;
+    class function New(const AProc: TProc<TArray<TValue>>;
+      const ACondition: Boolean): IBoolRouter; overload;
     procedure Execute(const ACondition: Boolean; const Args: TArray<TValue>);
   End;
 
@@ -36,12 +40,30 @@ begin
   FListMethods.Add(False, AFalse);
 end;
 
+constructor TBoolRouter.Create(const AProc: TProc<TArray<TValue>>;
+  const ACondition: Boolean);
+begin
+  FListMethods := TSmartPointer<TDictionary<Boolean, TProc<TArray<TValue>>>>.Create(
+    TDictionary<Boolean, TProc<TArray<TValue>>>.Create);
+  FListMethods.Add(ACondition, AProc);
+  FListMethods.Add(not ACondition,
+    procedure(Args: TArray<TValue>)
+    begin
+    end);
+end;
+
 procedure TBoolRouter.Execute(const ACondition: Boolean; const Args: TArray<TValue>);
 var
   lMethod: TProc<TArray<TValue>>;
 begin
   FListMethods.TryGetValue(ACondition, lMethod);
   lMethod(Args);
+end;
+
+class function TBoolRouter.New(const AProc: TProc<TArray<TValue>>;
+  const ACondition: Boolean): IBoolRouter;
+begin
+  Result := Self.Create(AProc, ACondition);
 end;
 
 class function TBoolRouter.New(const ATrue, AFalse: TProc<TArray<TValue>>): IBoolRouter;
